77520c1ead6f2295eac134d0bbe85f86b321e5ca
Jason <zacharyaars@gmail.com>
2024-03-02 08:24:47 -0700

    Update Words.cs

--- Data/Words.cs
+++ Data/Words.cs
@@ -4,10 +4,12 @@ namespace LabyrinthExplorer.Data
 {
     public class Words
     {
-        List<string> Adjectives = new List<string>(){
-            "dark",
-            "bloody",
-            "slimy"
+        List<string> Adjectives = new List<string>() { 
+            "dark", "musty", "dreary", "eerie", "mysterious", 
+            "atmospheric", "suspenseful", "chilling", "spooky", 
+            "creepy", "ghostly", "macabre", "frightening", 
+            "fearsome", "filthy", "foul", "nasty", "nightmarish", 
+            "sinister", "spindly", "spidery" 
         };
     }
 }
