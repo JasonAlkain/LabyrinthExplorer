f43370d45522bd220eff2175b9c58c9f79692846
Jason <zacharyaars@gmail.com>
2024-03-02 09:07:39 -0700

    Update GameplayData.cs
    Adding constructor

--- Gameplay/GameplayData.cs
+++ Gameplay/GameplayData.cs
@@ -9,11 +9,20 @@ namespace GameplayNamespace
     public class GameplayData
     {
         public static Room _Room;
-        public static int roomCount = 0;
+        public static int roomCount;
         public static CardType _RoomCard;
-        public static string _Input = "";
-        public static List<string> actions = new List<string>();
+        public static string _Input;
+        public static List<string> actions;
         public static List<Card> CardList;
 
+        public GameplayData()
+        {
+            _Room = new Room();
+            roomCount = 0;
+            _RoomCard = CardType.None;
+            _Input = string.Empty;
+            actions = new List<string>();
+            CardList = new List<Card>();
+        }
     }
 }
