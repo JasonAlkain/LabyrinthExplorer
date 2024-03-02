909ed8b154a8408607fd94192855b8bf91bd7746
Jason <zacharyaars@gmail.com>
2024-03-02 08:25:01 -0700

    Update Events.cs

--- Data/Events.cs
+++ Data/Events.cs
@@ -8,8 +8,7 @@ namespace LabyrinthExplorer.Data
                 "A fresh cent fills the room and you relax a little.",
                 "A chill runs down your back and you begin to feel unnerved.",
                 "The sounds of laughter fills the room and chills you to the core.",
-                "You search the room when you look up you see a faint image\n" +
-                " of a woman standing before you.\n" +
+                "You search the room when you look up you see a faint image of a woman standing before you." +
                 "You blink and she is gone."
             };
     }
