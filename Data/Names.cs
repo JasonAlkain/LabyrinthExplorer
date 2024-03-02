0849e5d38f08a243607f835acdb5dd2eb167f3d5
Jason <zacharyaars@gmail.com>
2024-03-02 08:59:50 -0700

    Create Names.cs

---
+++ Data/Names.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthExplorer.Data
+{
+    internal class Names
+    {
+        public List<string> genderNeutralNames = new List<string>
+        {
+            "Ava",
+            "Alex",
+            "Avery",
+            "Aiden",
+            "Aria",
+            "Aiden",
+            "Ari",
+            "Ayla",
+            "Asher",
+            "Avery",
+            "Aiden",
+            "Aria",
+            "Aiden",
+            "Ari",
+            "Ayla",
+            "Asher",
+            "Avery",
+            "Aiden",
+            "Aria",
+            "Aiden",
+            "Ari",
+            "Ayla",
+            "Asher",
+            "Bertie",
+            "Berti",
+            "Berty",
+            "Bhavya",
+            "Bill",
+            "Billi",
+            "Billie",
+            "Billy",
+            "Bird",
+            "Byrd",
+            "Blair",
+            "Blær",
+            "Blaer",
+            "Blaire",
+            "Bay",
+            "Baylor",
+            "Bee",
+            "Bennie",
+            "Bergen",
+            "Berkeley",
+            "Berlin",
+            "Bern",
+            "Berne",
+            "Bernie",
+            "Berni",
+            "Berny",
+            "Babe",
+            "Baby",
+            "Bae",
+            "Bai",
+            "Bailey",
+            "Bailee",
+            "Baileigh",
+            "Bao",
+            "Bentley",
+        };
+
+    }
+}
