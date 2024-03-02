df5bd8c5e81c2da5a840856754f0aa1ac2257331
Jason <zacharyaars@gmail.com>
2024-03-02 09:06:59 -0700

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -7,12 +7,33 @@ namespace LabyrinthExplorer
 {
     public class Player
     {
-        string Name { get; set; }
-        public static List<CardType> Cards;
+        public static string Name { get; set; }
+        public static int Sanity { get; set; }
+
+
+        //public static List<CardType> Cards;
         public static List<Card> Inventory;
+        
+        
+        public Player()
+        {
+            Name = "";
+            Sanity = 10;
+            Inventory = new List<Card>();
+        }
+        
         public static void ShowInvetory()
         {
             Inventory.ForEach(item => Utils.Print($"[ {item.Name} ] "));
+        }
+
+        public override string ToString()
+        {
+            string result;
+            result = $"{Name}";
+
+
+            return base.ToString();
         }
     }
 
