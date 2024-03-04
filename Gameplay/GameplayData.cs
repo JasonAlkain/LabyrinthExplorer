aeed63380b0d954bc4d9e5f2fd6eff9ac9a8db8b
Jason <zacharyaars@gmail.com>
2024-03-03 17:19:06 -0700

    Update GameplayData.cs

--- Gameplay/GameplayData.cs
+++ Gameplay/GameplayData.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Enums;
 using LabyrinthExplorer.Data;
 using RoomNamespace;
@@ -9,20 +10,33 @@ namespace GameplayNamespace
     public class GameplayData
     {
         public static Room _Room;
+        [Obsolete]
         public static int roomCount;
+        [Obsolete]
         public static CardType _RoomCard;
-        public static string _Input;
         public static List<string> actions;
         public static List<Card> CardList;
+
+        
+        public static Notifier<string> _Input;
+
+        public static string Input
+        {
+            get { return _Input.Prop; }
+            set { _Input.Prop = value; }
+        }
+
+
 
         public GameplayData()
         {
             _Room = new Room();
-            roomCount = 0;
-            _RoomCard = CardType.None;
-            _Input = string.Empty;
+            Input = string.Empty;
             actions = new List<string>();
             CardList = new List<Card>();
         }
+
+
+        public static void Printf(string s) => new Print(s);
     }
 }
